e96be203832ff69406dc563b6113f645bc579e77
Jaya Srikar <130632368+jayasrikark@users.noreply.github.com>
2023-08-09 18:18:34 +0530

    config: proj configuration

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-gray-800 text-white">
+      Hello world!
+    </div>
+  );
+}
+
+export default App;
